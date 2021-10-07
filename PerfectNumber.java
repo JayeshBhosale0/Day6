bbb6b2d50f31fd2caf432faecec014d8ec2de371
jayesh <jayesh.bhosale1812@gmail.com>
2021-10-07 19:36:36 +0530

    logical program

---
+++ PerfectNumber.java
@@ -0,0 +1,32 @@
+package newProject;
+import java.util.Scanner;
+public class PerfectNumber {
+	public static void main(String[] args) {
+	int n, sum = 0;
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter any integer you want to check:");
+    n = s.nextInt();
+    for(int i = 1; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    if(sum == n)
+    {
+        System.out.println("Given number is Perfect");
+    }
+    else
+    {
+        System.out.println("Given number is not Perfect");
+    }    
+}
+
+	 
+	
+	
+	}
+	
+
+
